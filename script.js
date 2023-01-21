5cce3e92d938dcc6f74fb7b678cfab84d9ff4932
james <jkuyt39@gmail.com>
2023-01-21 18:08:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ var returnFromCache = function(request){
   return caches.open("offline").then(function (cache) {
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
+       return cache.match("index.html");
      } else {
        return matching;
      }
